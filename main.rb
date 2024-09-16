0365e4715c5033b86c0a7fe7c7b328552ec58d04
boztopuz <boztopuz@gmail.com>
2024-09-16 18:18:30 +0300

    [BE-4430]: Made some improvements

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'open3'
+require 'fileutils'
 
 def env_has_key(key)
 	return (ENV[key] == nil || ENV[key] == "") ? nil : ENV[key]
@@ -22,14 +23,39 @@ def runCommand(command)
       raise stderr_str
     end
 end
-runCommand("brew tap tuist/tuist && brew install tuist")
+
+runCommand("brew install tap/tuist")
 options = ""
 tuist_path = env_has_key("AC_TUIST_PATH")
 project_only = env_has_key("AC_TUIST_PROJECT_ONLY") || "false"
+tuist_version = env_has_key("AC_TUIST_VERSION")
+tuist_build = env_has_key("AC_TUIST_BUILD") || "false"
+tuist_test = env_has_key("AC_TUIST_TEST") || "false"
+tuist_build_scheme = env_has_key("AC_TUIST_BUILD_SCHEME")
+tuist_test_scheme = env_has_key("AC_TUIST_TEST_SCHEME")
+tuist_fetch = env_has_key("AC_TUIST_FETCH") || "false"
+tuist_clean = env_has_key("AC_TUIST_CLEAN") || "false"
+
+
+if !tuist_version
+  runCommand("brew install tuist")
+else
+  runCommand("brew install tuist@#{tuist_version}")
+end
+
 if tuist_path
   options += " -p #{tuist_path}"
 end
 if project_only == "true"
   options += " -P"
 end
+
+puts "Tuist Installed. Running related Tuist commands."
+
+runCommand("tuist fetch") if tuist_fetch
 runCommand("tuist generate #{options}")
+runCommand("tuist clean #{options}") if tuist_clean
+runCommand("tuist build #{options}") if tuist_build
+runCommand("tuist test #{options}") if tuist_test
+runCommand("tuist build #{options} #{tuist_build_scheme}") if tuist_build_scheme
+runCommand("tuist test #{options} #{tuist_test_scheme}") if tuist_test_scheme
